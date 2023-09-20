7e95ed65b2b73d2e69d6f01f9290b0b873a509ba
bennynkongakd <bennyibmn@gmail.com>
2023-09-20 15:34:24 +0100

    pass token for all secured crud api calls

--- src/hooks/api/use-api-secured.ts
+++ src/hooks/api/use-api-secured.ts
@@ -1,14 +1,18 @@
+import { useContext, useEffect } from "react";
 import useApi from "./use-api";
+import { GlobalContext } from '../../contexts/context/global';
 
 export default function useApiSecured(path : string, baseRoute? : string) {
-    const {accessToken} = {accessToken : ''}
+    const {accessToken} = useContext(GlobalContext) // {accessToken : ''}
     const api = useApi(path)
 
-    api.addRequestInterceptor( config => {
-        config.headers = { ...config.headers, 'Authorization' : `Bearer ${accessToken}` }
-
-        return config
-    })
+    
+    useEffect( () => {        
+        api.addRequestInterceptor( config => {
+            config.headers = { ...config.headers, 'Authorization' : `Bearer ${accessToken}` }
+            return config
+        })
+    }, [accessToken, api])
 
     return api
 };
